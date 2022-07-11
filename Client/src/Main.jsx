4e447737d6b872a66317e587452f9caafc713cfd
MiniatureDev <onlyfordevstuff@proton.me>
2022-07-11 12:12:32 +0530

    Second commit - Initialized frontend server

---
+++ Client/src/Main.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+        <Router>
+            <h1>Main</h1>
+            <Routes>
+                <Route/>
+            </Routes>
+        </Router>
+    </div>
+  )
+}
+
+export default Main
